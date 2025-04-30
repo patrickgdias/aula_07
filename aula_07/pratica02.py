b41f0bf71bcd5503ddead2c5c0fef002eebb3cd9
patrickgdias <patrickgdias@outlook.com>
2025-04-29 21:34:08 -0300

    Ativiades 01 e pratica 02

---
+++ aula_07/pratica02.py
@@ -0,0 +1,24 @@
+def dobro(x):
+    d = x * 2
+    return d
+
+def triplo(x):
+    t = x * 3
+    return t
+
+def quadrado(x):
+    q = x**2
+    return q
+
+x = int(input('Digite um número'))
+print (dobro(x))
+print (triplo(x))
+print (quadrado(x))
+
+
+
+
+
+
+    
+    
